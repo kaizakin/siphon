8f9ebb26b5753f9c414ab995d035cb393ac26f21
karthik balasubramanian <karthikbalasubramanian08@gmail.com>
2026-06-09 01:22:49 +0530

    refactor: add database URL loading to config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,19 +1,29 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 type Config struct {
 	Port string
+	DB_URL string
 }
 
 func Load() Config {
 	port := os.Getenv("PORT")
+	dbUrl := os.Getenv("DATABASE_URL")
 
 	if port == "" {
 		port = "8080"
 	}
 
+	if dbUrl == "" {
+		log.Fatal("Database Url is not found!")
+	}
+
 	return Config{
 		Port: port,
+		DB_URL: dbUrl,
 	}
 }
