cd49bc80a7fcfc5b9672104ff37747e55ee49b57
karthik balasubramanian <karthikbalasubramanian08@gmail.com>
2026-06-23 01:37:56 +0530

    refactor: add env config loading to ingestion service

--- cmd/ingestion-svc/main.go
+++ cmd/ingestion-svc/main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+
+	ingestionv1 "github.com/kaizakin/siphon/gen/ingestion/v1"
+	server "github.com/kaizakin/siphon/internal/ingestion/grpc"
+	"github.com/kaizakin/siphon/pkg/config"
+)
+
+type Config struct {
+  Port string
+  KafkaBroker string
+}
+
 func main() {
-	
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error reading .env file")
+  }
+
+  cfg := Config{
+    Port: config.Getenv("PORT"),
+    KafkaBroker: config.Getenv("KAFKA_BROKER"),
+  }
+
+  lis, err := net.Listen("tcp", ":" + cfg.Port)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  grpcServer := grpc.NewServer()
+  ingestionserver := server.NewIngestionServer(cfg.KafkaBroker)
+  
+  ingestionv1.RegisterEventIngestionServiceServer(grpcServer, ingestionserver)
+
+  log.Printf("grpc server listening on port %s", cfg.Port)
+  log.Fatal(grpcServer.Serve(lis))
 }
