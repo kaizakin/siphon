0498814248245dc8ae23e171b6c5151f7d15d32f
karthik balasubramanian <karthikbalasubramanian08@gmail.com>
2026-06-23 01:38:07 +0530

    feat: add gRPC ingestion server with Kafka writer

---
+++ internal/ingestion/grpc/ingestion_server.go
@@ -0,0 +1,95 @@
+package grpcserver
+
+import (
+	"context"
+	"time"
+
+	ingestionv1 "github.com/kaizakin/siphon/gen/ingestion/v1"
+	"github.com/segmentio/kafka-go"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+)
+
+type IngestionServer struct {
+	ingestionv1.UnimplementedEventIngestionServiceServer
+	writer     *kafka.Writer
+	dlqWriter  *kafka.Writer
+	eventQueue chan *ingestionv1.IngestEventRequest
+}
+
+func NewIngestionServer(addr string) *IngestionServer {
+	s := &IngestionServer{
+		writer: &kafka.Writer{
+			Addr:     kafka.TCP(addr),
+			Topic:    "events",
+			Balancer: &kafka.LeastBytes{},
+		},
+		dlqWriter: &kafka.Writer{
+			Addr:     kafka.TCP(addr),
+			Topic:    "events-dlq",
+			Balancer: &kafka.LeastBytes{},
+		},
+		eventQueue: make(chan *ingestionv1.IngestEventRequest, 10000), // buffered channel that can hold 10,000 requests.
+	}
+
+	for i := 0; i < 10; i++ {
+		go s.kafkaWorker() //
+	}
+
+	return s
+}
+
+// kafka worker keeps on writing messages from the channel to kafka
+func (s *IngestionServer) kafkaWorker() {
+	for req := range s.eventQueue {
+		payload, err := proto.Marshal(req) // marshall the msg to protobuf binary format for transport
+		if err != nil {
+			continue
+		}
+
+		msg := kafka.Message{
+			Key:   []byte(req.CorrelationId),
+			Value: payload,
+		}
+
+		err = s.writer.WriteMessages(context.Background(), msg)
+		if err != nil {
+			s.writeToDLQ(msg)
+		}
+	}
+}
+
+func (s *IngestionServer) writeToDLQ(msg kafka.Message) {
+	for i := 0; i < 3; i++ {
+		err := s.dlqWriter.WriteMessages(context.Background(), msg)
+		if err == nil {
+			break
+		}
+
+		time.Sleep(time.Second) // wait for a second before trying again.
+	}
+}
+
+// ingestevent sends an optimistic acknowledgement as soon as the event reaches the buffered channel
+// this works because the worker handles the event producing & dlq writes
+func (s *IngestionServer) IngestEvent(ctx context.Context, req *ingestionv1.IngestEventRequest) (*ingestionv1.IngestEventResponse, error) {
+	select {
+	case s.eventQueue <- req:
+		return &ingestionv1.IngestEventResponse{
+			EventId: req.EventId,
+			Status:  "event accepted",
+		}, nil
+
+	default:
+		return nil, status.Error(codes.ResourceExhausted, "Ingestion queue is full")
+	}
+}
+
+func (s *IngestionServer) ListDLQEvents(ctx context.Context, req *ingestionv1.ListDLQEventsRequest) (*ingestionv1.ListDLQEventsResponse, error) {
+// TODO: configure postgres to store dlq events and return the pending dlq events as response   
+}
+
+func (s *IngestionServer) RetryDLQEvent(ctx context.Context, req *ingestionv1.RetryDLQEventRequest) (*ingestionv1.RetryDLQEventResponse, error) {
+// TODO: get the event by eventID push it back in the ingestion buffer
+}
